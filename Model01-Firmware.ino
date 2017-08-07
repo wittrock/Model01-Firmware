60ed8ed3e930d2f7696290c445b22963ccb7819e
John Wittrock <john@sidewalklabs.com>
2017-08-07 12:35:45 -0400

    PageUp -> tab, left Fn -> LeftControl

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -84,10 +84,10 @@ ___ \
 #define WITTROCK KEYMAP_STACKED ( \
     ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
     Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
-    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
     Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
     Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
-                          Key_Keymap1_Momentary,     \
+                          Key_LeftControl,     \
 \
     Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
     Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
