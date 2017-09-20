92a5484499aa25f9a0d61509b153cd785d7f6770
John Wittrock <john@sidewalklabs.com>
2017-09-20 11:28:07 -0400

    left ctrl -> space

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -86,7 +86,7 @@ ___ \
     Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
     Key_Tab,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
     Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
-    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
+    Key_Spacebar, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
                           Key_LeftControl,     \
 \
     Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
