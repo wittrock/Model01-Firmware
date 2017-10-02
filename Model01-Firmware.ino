b489c2d7524cab46fda794fb63ddd0d1566d6308
John Wittrock <john@sidewalklabs.com>
2017-10-02 09:58:12 -0400

    switch butterfly to enter

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -92,7 +92,7 @@ ___ \
     Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
     Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
                    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
-    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
+    Key_Enter,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
     Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_Enter,                   \
     Key_Keymap1_Momentary \
 )
